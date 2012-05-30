d20150afa36c1b35d5dc23e00c3b415b80a0574a
David Waitzman <djw@bbn.com>
2012-05-30 15:50:00 -0400

    Removed autogeneration of code from .def file from build process. Committed the already-generated files (ex. planning/src/org/cougaar/planning/ldm/asset/AssetFactory.java)

---
+++ planning/src/org/cougaar/planning/ldm/measure/Rate.java
@@ -0,0 +1,44 @@
+/*
+ * <copyright>
+ *  
+ *  Copyright 1997-2004 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects
+ *  Agency (DARPA).
+ * 
+ *  You can redistribute this software and/or modify it under the
+ *  terms of the Cougaar Open Source License as published on the
+ *  Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  
+ * </copyright>
+ */
+
+/* @generated Wed May 30 15:01:15 EDT 2012 from /Users/djw/unbackedup/cougaar-tmp/repo/planning/planning/src/org/cougaar/planning/ldm/measure/measures.def - DO NOT HAND EDIT */
+/** Immutable implementation of Rate.
+ **/
+
+
+package org.cougaar.planning.ldm.measure;
+import java.io.*;
+
+
+
+/** Implemented by Measures which represent derivatives with
+ * respect to Duration.
+ *
+ * Derivative.getDenominatorClass() will always
+ * return Duration
+ **/
+public interface Rate extends Derivative {
+}
